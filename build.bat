bcfa5ccb8b95c8273747682ccc493569107c4ac8
reg <entry.reg@gmail.com>
2016-08-29 01:29:58 +0300

    fixed CI build - forgotten packages.config

--- build.bat
+++ build.bat
@@ -23,7 +23,7 @@ goto exit
 
 echo MSBuild Tools: %msbuild%
 
-%msbuild% gnt.core /p:ngconfig="packages.config|RGiesecke.DllExport/packages.config|RGiesecke.DllExport.MSBuild/packages.config" /nologo /verbosity:m
+%msbuild% gnt.core /p:ngconfig="packages.config|NSBin/packages.config|RGiesecke.DllExport/packages.config|RGiesecke.DllExport.MSBuild/packages.config" /nologo /verbosity:m
 %msbuild% "DllExport.sln" /verbosity:normal /l:"packages\vsSBE.CI.MSBuild\bin\CI.MSBuild.dll" /m:8 /t:Build /p:Configuration=Release
 
 :exit
