3F/DllExport

commit ca1d73aa664da12bd4170af29772e8e1fca21b6f

Fixed bug - "An item with the same key has already been added" #10

RGiesecke.DllExport/Parsing/Actions/DeleteExportAttributeParserAction.cs
@@ -14,52 +14,75 @@ namespace RGiesecke.DllExport.Parsing.Actions
     {
         public override void Execute(ParserStateValues state, string trimmedLine)
         {
-            if(trimmedLine.StartsWith(".custom", StringComparison.Ordinal) || trimmedLine.StartsWith("// Code", StringComparison.Ordinal))
-            {
-                ExportedClass exportedClass;
-                if(this.Exports.ClassesByName.TryGetValue(state.ClassNames.Peek(), out exportedClass))
-                {
-                    ExportedMethod exportMethod = exportedClass.MethodsByName[state.Method.Name][0];
-                    string declaration = state.Method.Declaration;
-                    StringBuilder stringBuilder = new StringBuilder(250);
-                    stringBuilder.Append(".method ").Append(state.Method.Attributes.NullSafeTrim()).Append(" ");
-                    stringBuilder.Append(state.Method.Result.NullSafeTrim());
-                    stringBuilder.Append(" modopt(['mscorlib']'").Append(AssemblyExports.ConventionTypeNames[exportMethod.CallingConvention]).Append("') ");
-                    if(!string.IsNullOrEmpty(state.Method.ResultAttributes))
-                    {
-                        stringBuilder.Append(" ").Append(state.Method.ResultAttributes);
-                    }
-                    stringBuilder.Append(" '").Append(state.Method.Name).Append("'").Append(state.Method.After.NullSafeTrim());
-                    bool flag = this.ValidateExportNameAndLogError(exportMethod, state);
-                    if(flag)
-                    {
-                        state.Method.Declaration = stringBuilder.ToString();
-                    }
-                    if(state.MethodPos != 0)
-                    {
-                        state.Result.Insert(state.MethodPos, state.Method.Declaration);
-                    }
-                    if(flag)
-                    {
-                        this.Notifier.Notify(-2, DllExportLogginCodes.OldDeclaration, "\t" + Resources.OldDeclaration_0_, (object)declaration);
-                        this.Notifier.Notify(-2, DllExportLogginCodes.NewDeclaration, "\t" + Resources.NewDeclaration_0_, (object)state.Method.Declaration);
-                        state.Result.Add(string.Format((IFormatProvider)CultureInfo.InvariantCulture, "    .export [{0}] as '{1}'", new object[2]
-                        {
-                        (object) exportMethod.VTableOffset,
-                        (object) exportMethod.ExportName
-                        }));
-                        this.Notifier.Notify(-1, DllExportLogginCodes.AddingVtEntry, "\t" + Resources.AddingVtEntry_0_export_1_, (object)exportMethod.VTableOffset, (object)exportMethod.ExportName);
-                    }
-                }
-                else
-                {
-                    state.AddLine = false;
-                }
-                state.State = ParserState.Method;
-            }
-            else
+            if(!trimmedLine.StartsWith(".custom", StringComparison.InvariantCulture) // .custom instance void ['DllExport']'...'.'DllExportAttribute'::.ctor(string) = ( 01 00 06 50 72 69 6E 74 31 00 00 ) // ...Print1..
+                && !trimmedLine.StartsWith(".maxstack", StringComparison.InvariantCulture))
             {
                 state.AddLine = false;
+                return;
+            }
+            state.State = ParserState.Method;
+
+            ExportedClass exportedClass;
+            if(!Exports.ClassesByName.TryGetValue(state.ClassNames.Peek(), out exportedClass)) {
+                state.AddLine = false;
+                return;
+            }
+
+            ExportedMethod exportMethod = getExportedMethod(state, exportedClass);
+            string declaration          = state.Method.Declaration;
+            StringBuilder stringBuilder = new StringBuilder(250);
+
+            stringBuilder.Append(".method ").Append(state.Method.Attributes.NullSafeTrim()).Append(" ");
+            stringBuilder.Append(state.Method.Result.NullSafeTrim());
+            stringBuilder.Append(" modopt(['mscorlib']'").Append(AssemblyExports.ConventionTypeNames[exportMethod.CallingConvention]).Append("') ");
+
+            if(!String.IsNullOrEmpty(state.Method.ResultAttributes)) {
+                stringBuilder.Append(" ").Append(state.Method.ResultAttributes);
+            }
+
+            stringBuilder.Append(" '").Append(state.Method.Name).Append("'").Append(state.Method.After.NullSafeTrim());
+            bool flag = ValidateExportNameAndLogError(exportMethod, state);
+
+            if(flag) {
+                state.Method.Declaration = stringBuilder.ToString();
+            }
+
+            if(state.MethodPos != 0) {
+                state.Result.Insert(state.MethodPos, state.Method.Declaration);
+            }
+
+            if(flag)
+            {
+                Notifier.Notify(-2, DllExportLogginCodes.OldDeclaration, "\t" + Resources.OldDeclaration_0_, declaration);
+                Notifier.Notify(-2, DllExportLogginCodes.NewDeclaration, "\t" + Resources.NewDeclaration_0_, state.Method.Declaration);
+
+                state.Result.Add(
+                    String.Format(
+                        CultureInfo.InvariantCulture, 
+                        "    .export [{0}] as '{1}'",
+                        exportMethod.VTableOffset,
+                        exportMethod.ExportName
+                    )
+                );
+
+                Notifier.Notify(-1, DllExportLogginCodes.AddingVtEntry, "\t" + Resources.AddingVtEntry_0_export_1_, exportMethod.VTableOffset, exportMethod.ExportName);
+            }
+        }
+
+        private ExportedMethod getExportedMethod(ParserStateValues state, ExportedClass exportedClass)
+        {
+            var exportedMethods = exportedClass.MethodsByName[state.Method.Name];
+
+            // TODO: To get from zero index - it was in original code, as getting of first exported method.
+            //       However, to solve problem https://github.com/3F/DllExport/issues/10 I also added the `ExportName` instead of `MemberName` in AssemblyExports.cs
+            //       And I left it 'as is', but the good way to provide information about arguments, i.e.:
+            //       ~ 'Print'(int32 'a') cil managed   -> .export [0] as 'Print1'
+            //       ~ 'Print'(bool 'b') cil managed    -> .export [1] as 'Print2'
+            try {
+                return exportedMethods[0];
+            }
+            finally {
+                exportedMethods.RemoveAt(0); // moving in order by adding ^
             }
         }
     }

RGiesecke.DllExport/AssemblyExports.cs
@@ -114,14 +114,14 @@ namespace RGiesecke.DllExport
             var dictionary = new Dictionary<string, DuplicateExports>();
             foreach(ExportedClass exportedClass in ClassesByName.Values)
             {
-                List<ExportedMethod> exportedMethodList = new List<ExportedMethod>(exportedClass.Methods.Count);
+                var exportedMethodList = new List<ExportedMethod>(exportedClass.Methods.Count);
                 foreach(ExportedMethod method in exportedClass.Methods)
                 {
                     DuplicateExports duplicateExports;
                     if(!dictionary.TryGetValue(method.ExportName, out duplicateExports))
                     {
                         method.VTableOffset = num++;
-                        this.MethodsByExportName.Add(method.MemberName, method);
+                        MethodsByExportName.Add(method.ExportName, method); // #10 :: MemberName -> ExportName + see DeleteExportAttributeParserAction
                         dictionary.Add(method.ExportName, new DuplicateExports(method));
                     }
                     else
@@ -130,10 +130,12 @@ namespace RGiesecke.DllExport
                         duplicateExports.Duplicates.Add(method);
                     }
                 }
+
                 ExportedClass exportClassCopy = exportedClass;
                 exportedMethodList.ForEach((Action<ExportedMethod>)(m => exportClassCopy.Methods.Remove(m)));
                 exportedClass.Refresh();
             }
+
             foreach(DuplicateExports duplicateExports in dictionary.Values)
             {
                 if(duplicateExports.Duplicates.Count > 0)

commit a0aabf71f178d5490a79596497a206846304399c

Fixed bug with incorrect index if selected both platforms x86 + x64
broken after ca1d73a - #10

RGiesecke.DllExport/DllExportWeaver.cs
@@ -144,24 +144,22 @@ namespace RGiesecke.DllExport
 
         private void RunIlAsm(IlAsm ilAsm)
         {
-            using(this.GetNotifier().CreateContextName((object)this, "RunIlAsm"))
+            using(GetNotifier().CreateContextName(this, "RunIlAsm"))
             {
-                if(this.InputValues.Cpu == CpuPlatform.AnyCpu)
-                {
-                    string str = Path.GetDirectoryName(this.InputValues.OutputFileName) ?? "";
-                    string fileName = Path.GetFileName(this.InputValues.OutputFileName);
-                    if(!Directory.Exists(str))
-                    {
-                        throw new DirectoryNotFoundException(string.Format(Resources.Directory_0_does_not_exist, (object)str));
-                    }
+                if(InputValues.Cpu != CpuPlatform.AnyCpu) {
+                    reassembleFile(ilAsm, InputValues.OutputFileName, "", InputValues.Cpu);
+                    return;
+                }
 
-                    ilAsm.ReassembleFile(Path.Combine(Path.Combine(str, "x86"), fileName), ".x86", CpuPlatform.X86);
-                    ilAsm.ReassembleFile(Path.Combine(Path.Combine(str, "x64"), fileName), ".x64", CpuPlatform.X64);
+                string dir      = Path.GetDirectoryName(InputValues.OutputFileName) ?? "";
+                string fileName = Path.GetFileName(InputValues.OutputFileName);
+
+                if(!Directory.Exists(dir)) {
+                    throw new DirectoryNotFoundException(String.Format(Resources.Directory_0_does_not_exist, dir));
                 }
-                else
-                {
-                    ilAsm.ReassembleFile(this.InputValues.OutputFileName, "", this.InputValues.Cpu);
-                }
+
+                reassembleFile(ilAsm, Path.Combine(Path.Combine(dir, "x86"), fileName), ".x86", CpuPlatform.X86);
+                reassembleFile(ilAsm, Path.Combine(Path.Combine(dir, "x64"), fileName), ".x64", CpuPlatform.X64);
             }
         }
 
@@ -189,6 +187,23 @@ namespace RGiesecke.DllExport
                     ilDasm2.TempDirectory = tempDirectory;
                     ilDasm2.Run();
                 }
+            }
+        }
+
+        private int reassembleFile(IlAsm ilAsm, string outputFile, string ilSuffix, CpuPlatform cpu)
+        {
+            try {
+                return ilAsm.ReassembleFile(outputFile, ilSuffix, cpu);
+            }
+            finally {
+                cleanup(ilAsm);
+            }
+        }
+
+        private void cleanup(IlAsm ilAsm)
+        {
+            foreach(var _class in ilAsm.Exports.ClassesByName.Values) {
+                _class.resetExportedMethods();
             }
         }
     }

RGiesecke.DllExport/ExportedClass.cs
@@ -2,14 +2,19 @@
 // Author of original assembly (MIT-License): Robert Giesecke
 // Use Readme & LICENSE files for details.
 
+using System;
 using System.Collections.Generic;
 
 namespace RGiesecke.DllExport
 {
+    using EMIndex = Dictionary<string, int>;
+
     public class ExportedClass
     {
         private readonly List<ExportedMethod> _Methods = new List<ExportedMethod>();
         private readonly Dictionary<string, List<ExportedMethod>> _MethodsByName = new Dictionary<string, List<ExportedMethod>>();
+
+        private EMIndex emOrder = new EMIndex();
 
         public string FullTypeName
         {
@@ -58,6 +63,39 @@ namespace RGiesecke.DllExport
                     local_1.Add(item_0);
                 }
             }
+        }
+
+        /// <summary>
+        /// 
+        /// TODO: To get from zero index - it was in original code, as getting of first exported method.
+        ///       However, to solve problem https://github.com/3F/DllExport/issues/10 I also added the `ExportName` instead of `MemberName` in AssemblyExports.cs
+        ///       And I left it 'as is', but the good way to provide information about arguments, i.e.:
+        ///       ~ 'Print'(int32 'a') cil managed   -> .export [0] as 'Print1'
+        ///       ~ 'Print'(bool 'b') cil managed    -> .export [1] as 'Print2'
+        ///       
+        /// this also affects on DllExportWeaver.RunIlAsm
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        internal ExportedMethod nextExportedMethod(string name)
+        {
+            if(String.IsNullOrWhiteSpace(name)) {
+                throw new ArgumentException("The name cannot be null or empty for EMIndex.");
+            }
+
+            if(!emOrder.ContainsKey(name)) {
+                emOrder[name] = 0;
+            }
+            return MethodsByName[name][emOrder[name]++]; // moving in order by adding
+        }
+
+        internal void resetExportedMethods()
+        {
+            if(emOrder == null) {
+                emOrder = new EMIndex();
+                return;
+            }
+            emOrder.Clear();
         }
     }
 }

RGiesecke.DllExport/Parsing/Actions/DeleteExportAttributeParserAction.cs
@@ -71,19 +71,8 @@ namespace RGiesecke.DllExport.Parsing.Actions
 
         private ExportedMethod getExportedMethod(ParserStateValues state, ExportedClass exportedClass)
         {
-            var exportedMethods = exportedClass.MethodsByName[state.Method.Name];
-
-            // TODO: To get from zero index - it was in original code, as getting of first exported method.
-            //       However, to solve problem https://github.com/3F/DllExport/issues/10 I also added the `ExportName` instead of `MemberName` in AssemblyExports.cs
-            //       And I left it 'as is', but the good way to provide information about arguments, i.e.:
-            //       ~ 'Print'(int32 'a') cil managed   -> .export [0] as 'Print1'
-            //       ~ 'Print'(bool 'b') cil managed    -> .export [1] as 'Print2'
-            try {
-                return exportedMethods[0];
-            }
-            finally {
-                exportedMethods.RemoveAt(0); // moving in order by adding ^
-            }
+            //TODO: see details in nextExportedMethod()
+            return exportedClass.nextExportedMethod(state.Method.Name);
         }
     }
 }