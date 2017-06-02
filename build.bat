2f756c1c55634afc83ed0de74a0c63e9e54e9818
Denis Kuzmin (reg) <entry.reg@gmail.com>
2017-06-02 13:39:56 +0300

    PublicRelease cfg for additional control of ConfVersion* - #31
    -*now it's part of this repo: SHA1 & rev will be updated only when PublicRelease cfg is used

--- build.bat
+++ build.bat
@@ -1,13 +1,19 @@
 @echo off
 
+set reltype=%1
 set cimdll=packages\vsSBE.CI.MSBuild\bin\CI.MSBuild.dll
 set _msbuild=tools\hMSBuild
+
+if "%reltype%"=="" (
+    set reltype=Release
+)
 
 :found
 
 call packages_restore.cmd || goto err
 
-%_msbuild% "DllExport.sln" /v:normal /l:"%cimdll%" /m:4 /t:Build /p:Configuration=Release || goto err
+call %_msbuild% -notamd64 "DllExport.sln" /v:normal /l:"%cimdll%" /m:4 /t:Build /p:Configuration="%reltype%" || goto err
+
 exit /B 0
 
 :err
