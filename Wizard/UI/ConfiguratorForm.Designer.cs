0261a1ac8e0e996d30da11ef3448352c5dec01e7
Denis Kuzmin [ GitHub/3F ] <x-3F@outlook.com>
2019-12-11 23:08:06 +0300

    Fixed error when switching between .sln in UI. Wizard.

--- Wizard/UI/ConfiguratorForm.Designer.cs
+++ Wizard/UI/ConfiguratorForm.Designer.cs
@@ -16,6 +16,7 @@
             if(disposing && (components != null)) {
                 components.Dispose();
             }
+            fdialog?.Dispose();
             base.Dispose(disposing);
         }
 
