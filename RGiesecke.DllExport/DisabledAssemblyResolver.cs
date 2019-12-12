e5366dbad9452b4e3b4f327d1ba5ff10415ca51f
Denis Kuzmin [ GitHub/3F ] <x-3F@outlook.com>
2019-12-12 21:20:16 +0300

    Fixed #127. 'Failed to resolve assembly...' when .NET Standard

---
+++ RGiesecke.DllExport/DisabledAssemblyResolver.cs
@@ -0,0 +1,25 @@
+﻿//# Author of original code ([Decompiled] MIT-License): Copyright (c) 2009-2015  Robert Giesecke
+//# Use Readme & LICENSE files for details.
+
+//# Modifications: Copyright (c) 2016-2019  Denis Kuzmin < entry.reg@gmail.com > GitHub/3F
+//$ Distributed under the MIT License (MIT)
+
+using Mono.Cecil;
+
+namespace RGiesecke.DllExport
+{
+    /// <summary>
+    /// Implements disabled assembly resolving.
+    /// Related issue: https://github.com/3F/DllExport/issues/127
+    /// 
+    /// For issue127 we can also use DefaultAssemblyResolver.AddSearchDirectory(),
+    /// But we don't actually need any assembly resolving in our case at all.
+    /// </summary>
+    internal sealed class DisabledAssemblyResolver: DefaultAssemblyResolver
+    {
+        public override AssemblyDefinition Resolve(AssemblyNameReference name)
+        {
+            return null; // It will be considered as `resolved = false` due to internal ResolutionException processing in cecil.
+        }
+    }
+}
