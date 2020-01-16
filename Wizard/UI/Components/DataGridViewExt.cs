163554f01162562127eb95f381e88f4db18908af
Denis Kuzmin [ GitHub/3F ] <x-3F@outlook.com>
2020-01-17 02:07:22 +0300

    Updating license headers for 1.7 changes

--- Wizard/UI/Components/DataGridViewExt.cs
+++ Wizard/UI/Components/DataGridViewExt.cs
@@ -1,7 +1,7 @@
 ﻿/*
  * The MIT License (MIT)
  * 
- * Copyright (c) 2016-2019  Denis Kuzmin < entry.reg@gmail.com > GitHub/3F
+ * Copyright (c) 2016-2020  Denis Kuzmin < x-3F@outlook.com > GitHub/3F
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,7 +24,7 @@
 
 /*
  * Modified. Original from https://github.com/3F/vsSolutionBuildEvent
- * Copyright (c) 2013-2016,2019  Denis Kuzmin < entry.reg@gmail.com > GitHub/3F
+ * Copyright (c) 2013-2016,2019  Denis Kuzmin < x-3F@outlook.com > GitHub/3F
 */
 
 using System;
